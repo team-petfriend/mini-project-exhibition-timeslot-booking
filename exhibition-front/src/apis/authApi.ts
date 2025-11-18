759b2817bd6d852a29a29e9e89a781b56e0952e3
tndals11 <136726862+tndals11@users.noreply.github.com>
2025-11-18 16:05:04 +0900

    2025-11-18 React route-settings
    path 경로설정

--- exhibition-front/src/apis/authApi.ts
+++ exhibition-front/src/apis/authApi.ts
@@ -3,7 +3,7 @@
 import type { LoginRequest, LoginResponse } from "@/types/auth.type";
 import { publicApi } from "./common/axiosInstance";
 import { API_ROUTES } from "./common/apiMappingPattern";
-import type { ApiResponse } from "@/types/ApiResponse";
+import type { ApiResponse } from "@/types/common/ApiResponse";
 
 // 로그인 요청
 export async function login(req: LoginRequest): Promise<LoginResponse> {
--- exhibition-front/src/apis/common/axiosInstance.ts
+++ exhibition-front/src/apis/common/axiosInstance.ts
@@ -1,39 +1,29 @@
 import axios, { type InternalAxiosRequestConfig } from "axios";
 
-const API_BASE =
-  import.meta.env.VITE_API_BASE || "http://localhost:8080/api/v1";
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8080';
 
 export const publicApi = axios.create({
   baseURL: API_BASE,
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    "Content-Type" : "application/json",
+    Accept: "application/json"
   },
-});
+  withCredentials: true
+})
 
 export const privateApi = axios.create({
-  baseURL: API_BASE,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  withCredentials: true,
+
 });
 
-// 요청(private)
-privateApi.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    return config;
-  },
-  (e) => Promise.reject(e)
-);
+privateApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
 
-// 응답(private)
+  return config;
+}, (e) => Promise.reject(e));
+
 privateApi.interceptors.response.use(
-  (response) => response,
+  response => response,
   async (e) => {
     return Promise.reject(e);
   }
-);
+)
