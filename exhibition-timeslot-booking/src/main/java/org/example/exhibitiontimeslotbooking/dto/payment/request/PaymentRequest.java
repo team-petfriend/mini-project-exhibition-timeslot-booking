41687c6b231b6bb560bf595b3dfff2e29a5c4417
jihoonjeong56 <jeong283056@gmail.com>
2025-12-03 14:12:07 +0900

    fix payment status and add file enum payment method

--- exhibition-timeslot-booking/src/main/java/org/example/exhibitiontimeslotbooking/dto/payment/request/PaymentRequest.java
+++ exhibition-timeslot-booking/src/main/java/org/example/exhibitiontimeslotbooking/dto/payment/request/PaymentRequest.java
@@ -1,6 +1,19 @@
 package org.example.exhibitiontimeslotbooking.dto.payment.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.example.exhibitiontimeslotbooking.common.enums.payment.PaymentMethod;
+
 public record PaymentRequest(
-        Long id
+        @NotBlank(message = "예약 id는 필수입니다.")
+        String bookingId,
+
+        @NotNull(message = "결제 금액은 필수입니다.")
+        @Min(value = 100, message = "최소 결제 금액은 100원입니다.")
+        Long amount,
+
+        @NotNull(message = "결제 수단은 필수입니다.")
+        PaymentMethod method
 ) {
 }
